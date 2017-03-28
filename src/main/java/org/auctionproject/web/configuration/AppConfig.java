6d38ecaff961f27978ebcd27095e1e12f363cc5f
Ravindra Dhakal <r.yatri7@gmail.com>
2017-03-27 23:39:21 -0500

    tiles working

--- src/main/java/org/auctionproject/web/configuration/AppConfig.java
+++ src/main/java/org/auctionproject/web/configuration/AppConfig.java
@@ -11,7 +11,9 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 /**
@@ -39,12 +41,22 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return messageSource;
     }
 
+    @Bean(name = "viewResolver")
+    public ViewResolver getViewResolver() {
+        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+
+        // TilesView 3
+        viewResolver.setViewClass(TilesView.class);
+
+        return viewResolver;
+    }
+
     @Bean
     public TilesConfigurer tilesConfigurer(){
         TilesConfigurer tilesConfigurer = new TilesConfigurer();
         tilesConfigurer.setDefinitions(new String[]{"WEB-INF/tiles.xml"});
         tilesConfigurer.setCheckRefresh(true);
-        tilesConfigurer.setCompleteAutoload(true);
+        //tilesConfigurer.setCompleteAutoload(true);
         return tilesConfigurer;
     }
     @Override
