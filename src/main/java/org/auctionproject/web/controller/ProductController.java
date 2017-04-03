cd7c88d3734026c4c3008aae28a586c4f1f22b70
Bishal Paudel <emailofbishal@gmail.com>
2017-04-03 16:59:25 -0500

    Products listing view basic is done. Pagination from url is working. Example: /?page=2&size=20&direction=price,asc Controller and View for product create is started, not implemented.

--- src/main/java/org/auctionproject/web/controller/ProductController.java
+++ src/main/java/org/auctionproject/web/controller/ProductController.java
@@ -1,19 +1,36 @@
 package org.auctionproject.web.controller;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.auctionproject.web.model.Product;
+import org.auctionproject.web.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Collection;
 
 /**
  * Created by bishal on 3/27/17.
  */
 @Controller
+@RequestMapping(value = "/products/")
+@EnableSpringDataWebSupport
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String list(Model model, Pageable pageRequest) {
+        Page<Product> products = productService.getAllActiveProducts(pageRequest);
+        model.addAttribute("products", products);
+        return "index";
+    }
 
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create() {
+        return "create";
+    }
 }
---
+++ src/main/webapp/WEB-INF/tags/util/pagination.tag
@@ -0,0 +1,1 @@
+<%-- TODO: Implement TagLib for pagination for Page object --%>
